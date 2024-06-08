13d041b09491bea2c00a83605e05a6af210b8ff8
lisa jones <lisadjones75@gmail.com>
2024-06-08 12:08:47 +0000

    Added home app and templates

--- gift_nook/settings.py
+++ gift_nook/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
 ]
 
@@ -61,7 +62,10 @@ ROOT_URLCONF = 'gift_nook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
