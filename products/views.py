358bec0fe22cfc969b3c83eb9eb4f43e6f037adf
lisa jones <lisadjones75@gmail.com>
2024-06-13 15:46:08 +0000

    Added sorting function

--- products/views.py
+++ products/views.py
@@ -10,8 +10,29 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
- 
+    sort = None
+    direction = None
 
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+ 
+    
 
     if request.GET:
         if 'q' in request.GET:
@@ -23,13 +44,17 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    context = {
-        'products': products,
-        'search_term': query,
-        
-    }
+        current_sorting = f'{sort}_{direction}'           
 
-    return render(request, 'products/products.html', context)
+        context = {
+            'products': products,
+            'search_term': query,
+            'current_categories': categories,
+            'current_sorting': current_sorting,
+            
+        }
+
+        return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
