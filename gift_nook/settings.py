d89f552d80804d170a126fb6369d02592f47b2ed
lisa jones <lisadjones75@gmail.com>
2024-06-20 11:20:10 +0000

    Add bag context and delivery logic

--- gift_nook/settings.py
+++ gift_nook/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.context.bag_contents',
             ],
         },
     },
@@ -158,3 +159,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STANDARD_DELIVERY_PERCENTAGE = 10
+FREE_DELIVERY_THRESHOLD = 50
