9e24fd01d5a6aad1cb4e30012009fc1933340442
Lisa Jones <lisadjones75@gmail.com>
2024-07-15 11:14:03 +0000

    Added loading overlay during payment process

--- checkout/models.py
+++ checkout/models.py
@@ -33,7 +33,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -68,5 +68,3 @@ class OrderLineItem(models.Model):
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
-    def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
