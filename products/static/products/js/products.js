1563422ee18a992f6154b50c6f5ab9bc514ed5cc
Lisa Jones <lisadjones75@gmail.com>
2024-09-07 14:04:11 +0000

    Add delete confirmationmodal when delete product

---
+++ products/static/products/js/products.js
@@ -0,0 +1,32 @@
+$('.btt-link').click(function(e) {
+    window.scrollTo(0,0)
+})
+
+
+$('.btn-delete').on('click', function(e) {
+    e.preventDefault(); // Prevent the default link action
+    var productId = $(this).data('product-id'); // Get the product ID from the data attribute
+    $('#deleteModal-' + productId).modal('show'); // Show the corresponding modal
+});
+
+
+$('#sort-selector').change(function() {
+    var selector = $(this);
+    var currentUrl = new URL(window.location);
+
+    var selectedVal = selector.val();
+    if(selectedVal != "reset"){
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+
+        window.location.replace(currentUrl);
+    }
+})
